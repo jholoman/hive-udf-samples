ea663cb3eda8251d4fe02b43986edcaf540232d0
jholoman <jeff.holoman@gmail.com>
2015-01-21 14:51:37 -0500

    Fixed log.info

--- java/src/main/java/com/cloudera/FuzzyEqualsUdf.java
+++ java/src/main/java/com/cloudera/FuzzyEqualsUdf.java
@@ -11,7 +11,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package com.cloudera.impala;
+package com.cloudera;
 
 import org.apache.hadoop.hive.ql.exec.UDF;
 import org.apache.hadoop.hive.serde2.io.ByteWritable;
